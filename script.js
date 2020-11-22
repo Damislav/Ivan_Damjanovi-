f752ccd25517c0356279de1bf666e9ed11058b59
Damislav <idamjanovic28@gmail.com>
2020-11-22 16:39:17 +0100

    sadsa

---
+++ script.js
@@ -0,0 +1,79 @@
+// making slider go brm brm -------------
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// end of slide ---------------
+
+// slide 2
+let slideContainer = 1;
+GetshowSlides(slideContainer);
+
+function GetplusSlides(arg) {
+  GetshowSlides((slideContainer += arg));
+}
+
+function GetcurrentSlide(arg) {
+  GetshowSlides((slideContainer = arg));
+}
+
+function GetshowSlides(arg) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides-2");
+  let dots = document.getElementsByClassName("dot-2");
+  if (arg > slides.length) {
+    slideContainer = 1;
+  }
+  if (arg < 1) {
+    slideContainer = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-2", "");
+  }
+  slides[slideContainer - 1].style.display = "block";
+  dots[slideContainer - 1].className += " active-2";
+}
+//  end of slide 2
+
+//  aos
+
+AOS.init({
+  offset: 400, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  once: true, // appear only once
+});
+
+//cookies
---
+++ to_Top.js
@@ -0,0 +1,53 @@
+// $(".mobile_nav").click(function () {
+//   var mm = $(".mobile_menu"),
+//     mn = $(".mobile_nav"),
+//     a = "active";
+
+//   if (mm.hasClass(a) && mn.hasClass(a)) {
+//     mm.removeClass(a).fadeOut(200);
+//     mn.removeClass(a);
+//     $(".mobile_menu li").each(function () {
+//       $(this).removeClass("slide");
+//     });
+//   } else {
+//     mm.addClass(a).fadeIn(200);
+//     mn.addClass(a);
+//     $(".mobile_menu li").each(function (i) {
+//       var t = $(this);
+//       setTimeout(function () {
+//         t.addClass("slide");
+//       }, (i + 1) * 100);
+//     });
+//   }
+// });
+
+// dropdown nav
+let button = document.querySelector(".mobile_nav");
+let dropdown = document.querySelector(".mobile_menu");
+
+button.addEventListener("click", () => {
+  dropdown.classList.toggle("display");
+  button.classList.toggle();
+});
+
+//Get the button to top
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
